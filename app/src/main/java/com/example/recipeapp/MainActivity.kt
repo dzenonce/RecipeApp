2339f5eec808a22af34e37f089affddb62fbe837
dzenonce <markin-dmitrij@yandex.ru>
2022-12-27 21:09:45 +0300

    RA-05 Реализовал показ фрагментов Категории и Избранное по нажитию на кнопку

--- app/src/main/java/com/example/recipeapp/MainActivity.kt
+++ app/src/main/java/com/example/recipeapp/MainActivity.kt
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.fragment.app.add
 import androidx.fragment.app.commit
+import androidx.fragment.app.replace
 import com.example.recipeapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -22,6 +23,36 @@ class MainActivity : AppCompatActivity() {
                 setReorderingAllowed(true)
                 add<CategoriesListFragment>(R.id.frgMainFragmentContainer)
             }
+        }
+
+        with(binding) {
+
+            btnCategoryButton.setOnClickListener {
+                showCategories()
+            }
+
+            btnFavoriteButton.setOnClickListener {
+                showFavorites()
+            }
+        }
+
+    }
+
+    fun showCategories() {
+        supportFragmentManager.commit {
+            setReorderingAllowed(true)
+            replace<CategoriesListFragment>(R.id.frgMainFragmentContainer)
+            setReorderingAllowed(true)
+            addToBackStack(null)
+        }
+
+    }
+
+    fun showFavorites() {
+        supportFragmentManager.commit {
+            replace<FavoritesFragment>(R.id.frgMainFragmentContainer)
+            setReorderingAllowed(true)
+            addToBackStack(null)
         }
 
     }
