1201fbccafabd4ab6cc257f2083fb50e68a1a03e
dzenonce <markin-dmitrij@yandex.ru>
2023-01-17 22:57:20 +0300

    RA-12 Realize data transfer with parcelable

--- app/src/main/java/com/example/recipeapp/Constants.kt
+++ app/src/main/java/com/example/recipeapp/Constants.kt
@@ -3,3 +3,4 @@ package com.example.recipeapp
 const val ARG_CATEGORY_ID: String = "arg_category_id"
 const val ARG_CATEGORY_NAME: String = "arg_category_name"
 const val ARG_CATEGORY_IMAGE_URL: String = "arg_category_image_url"
+const val ARG_RECIPE: String = "arg_recipe"
--- app/src/main/java/com/example/recipeapp/fragment/RecipeFragment.kt
+++ app/src/main/java/com/example/recipeapp/fragment/RecipeFragment.kt
@@ -1,19 +1,44 @@
 package com.example.recipeapp.fragment
 
+import android.os.Build
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import com.example.recipeapp.R
+import com.example.recipeapp.ARG_RECIPE
+import com.example.recipeapp.databinding.FragmentRecipeBinding
+import com.example.recipeapp.model.Recipe
 
 class RecipeFragment : Fragment() {
+
+    private var _binding: FragmentRecipeBinding? = null
+    private val binding
+        get() = _binding ?: throw IllegalStateException("Recipe Fragment Binding must not be null")
+
+    private var recipe: Recipe? = null
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        return inflater.inflate(R.layout.fragment_recipe, container, false)
+    ): View {
+        _binding = FragmentRecipeBinding.inflate(layoutInflater)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        arguments?.let {
+            recipe =
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU)
+                    it.getParcelable(ARG_RECIPE, Recipe::class.java)
+                else
+                    it.getParcelable(ARG_RECIPE)
+        }
+        with(binding) {
+            this.tvRecipeIdentifier.text = recipe?.title
+        }
     }
 
 }
