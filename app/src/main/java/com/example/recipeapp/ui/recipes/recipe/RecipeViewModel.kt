12bc607a46675e1d0f10e5ab8f3c5491fca9e773
dzenonce <markin-dmitrij@yandex.ru>
2023-02-17 20:17:25 +0300

    RA-22 Observer

--- app/src/main/java/com/example/recipeapp/ui/recipes/recipe/RecipeViewModel.kt
+++ app/src/main/java/com/example/recipeapp/ui/recipes/recipe/RecipeViewModel.kt
@@ -1,19 +1,26 @@
 package com.example.recipeapp.ui.recipes.recipe
 
+import android.util.Log
+import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModel
-import com.example.recipeapp.model.Ingredient
 import com.example.recipeapp.model.Recipe
+
+data class RecipeUiState(
+    var isFavorite: Boolean = false,
+    var recipe: Recipe? = null,
+    var portionsCount: Int = 1,
+)
 
 class RecipeViewModel: ViewModel() {
 
-    data class RecipeUiState(
-        var isFavorite: Boolean = false,
-        var recipe: Recipe? = null,
-        var portionCount: List<String> = listOf(),
-    )
-
     private val _uiState = MutableLiveData<RecipeUiState>()
-    val uiState: MutableLiveData<RecipeUiState> = _uiState
+    val uiState: LiveData<RecipeUiState> = _uiState
+
+    fun observe() {
+        _uiState.value = RecipeUiState(isFavorite = true)
+        Log.i("!!!", "From viewModel: ${uiState.value?.isFavorite}")
+    }
 
 }
