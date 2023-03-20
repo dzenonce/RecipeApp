253bed87edf52b1d0422b3c021940aeb39b77fdf
dzenonce <markin-dmitrij@yandex.ru>
2023-03-20 10:35:51 +0300

    RA-29 Navigation SafeArgs work

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,14 +4,3 @@ plugins {
     id("org.jetbrains.kotlin.android") version "1.9.10" apply false
     id("androidx.navigation.safeargs.kotlin") version "2.7.7" apply false
 }
-
-buildscript {
-    repositories {
-        google()
-    }
-
-    dependencies {
-        classpath("androidx.navigation.safeargs.kotlin:androidx.navigation.safeargs.kotlin.gradle.plugin:2.7.7")
-    }
-}
-
