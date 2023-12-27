d980903518ca4a6be101a4c7736753f4446b2758
dzenonce <markin-dmitrij@yandex.ru>
2023-12-27 13:34:07 +0300

    RA-04 Добавил фрагменты

---
+++ app/src/main/java/com/example/recipeapp/CategoriesListFragment.kt
@@ -0,0 +1,26 @@
+package com.example.recipeapp
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.recipeapp.databinding.FragmentListCategoriesBinding
+
+class CategoriesListFragment : Fragment() {
+
+    private var _binding: FragmentListCategoriesBinding? = null
+    private val binding
+        get() = _binding ?: throw IllegalStateException("Fragment CategoryListBinding must not be null")
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+
+        _binding = FragmentListCategoriesBinding.inflate(layoutInflater)
+
+        return binding.root
+    }
+
+}
--- app/src/main/java/com/example/recipeapp/MainActivity.kt
+++ app/src/main/java/com/example/recipeapp/MainActivity.kt
@@ -6,12 +6,21 @@ import com.example.recipeapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
-    private lateinit var binding: ActivityMainBinding
+    private var _binding: ActivityMainBinding? = null
+    private val binding
+        get() = _binding ?: throw IllegalStateException("ActivityMainBinding must not be null")
+
+    private val fragmentManager = supportFragmentManager
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = ActivityMainBinding.inflate(layoutInflater)
+        _binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        val transactionManager = fragmentManager.beginTransaction()
+        transactionManager.add(R.id.frgMainFragmentContainer, CategoriesListFragment())
+        transactionManager.commit()
+
     }
+
 }
