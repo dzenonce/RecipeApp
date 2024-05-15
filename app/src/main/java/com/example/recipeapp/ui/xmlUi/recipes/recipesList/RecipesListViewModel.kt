328328b8bc9b3e701413b90fb9d1f8b579a0912c
Dmitrii Markin <markin-dmitrij@yandex.ru>
2024-05-15 16:21:14 +0300

    RA-43 Refactor

--- app/src/main/java/com/example/recipeapp/ui/xmlUi/recipes/recipesList/RecipesListViewModel.kt
+++ app/src/main/java/com/example/recipeapp/ui/xmlUi/recipes/recipesList/RecipesListViewModel.kt
@@ -4,7 +4,6 @@ import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
-import com.example.recipeapp.API_RECIPE_IMAGE_URL
 import com.example.recipeapp.data.RecipeRepository
 import com.example.recipeapp.model.Recipe
 import kotlinx.coroutines.CoroutineExceptionHandler
